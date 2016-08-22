5bf9042be96304cf486da7f1e79af073ff747aaa
JimCampagno <jimcampagno@gmail.com>
2016-08-22 13:34:40 -0400

    Include Xcode Project

---
+++ ViewFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewFun
+//
+//  Created by James Campagno on 8/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}
